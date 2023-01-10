8c8700e2af4acb775d16e092cd05ebe2cb6033cb
Nataliia Fedorets <nataliiafedorets@gmail.com>
2023-01-10 23:21:00 +0000

    supplemented home work 09 with optional tasks

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,7 @@
+# визначення числа в послідовності Фібоначчі, що відповідає прийнятому індексу:
 index = int(input("Enter an index of a number in Fibonacci sequence: "))
 
+# використовуючи генератори:
 def fibonacci_func(index):
     num1 = 0
     num2 = 1
@@ -24,3 +26,23 @@ for i in fibonacci_gen:
     number = i
 print(number)
 
+# використовуючи рекурсію:
+def fibonacci_func(index):
+    if index == 0:
+        return 0
+    elif index == 1:
+        return 1
+    return fibonacci_func(index-1) + fibonacci_func(index-2)
+
+number = fibonacci_func(index)
+print(number)
+
+
+# знаходження факторіалу введеного числа, використовуючи рекурсію:
+n = int(input("Enter a number to find its factorial: "))
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+print(factorial(n))
