cc18f53d2d1ae033bfc8e992a0ce974011d56493
NataliiaFed <nataliiafedorets@gmail.com>
2023-04-23 17:13:14 +0100

    resolved some conflicts

--- django/book/models.py
+++ django/book/models.py
@@ -8,9 +8,6 @@ class PublishingHouse(models.Model):
     class Meta:
         db_table = 'publishing_house'
 
-    def __str__(self):
-        return f"{self.id}: {self.name} ({self.rating})"
-
 
 class Book(models.Model):
     title = models.CharField(max_length=255, null=False)
@@ -22,6 +19,3 @@ class Book(models.Model):
     class Meta:
         db_table = 'book'
         unique_together = ('title', 'author')
-
-    def __str__(self):
-        return f'{self.id}: "{self.title}" {self.author} {self.year} {self.publish_house.name} - {self.price}'
