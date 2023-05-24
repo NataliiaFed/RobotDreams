d5abd09a0b34c4c4e94ede3693f0c17915e07b15
NataliiaFed <nataliiafedorets@gmail.com>
2023-05-24 21:27:08 +0100

    home work 48

--- django/user/tests.py
+++ django/user/tests.py
@@ -1,3 +1,75 @@
-from django.test import TestCase
+import factory
+import random
 
-# Create your tests here.
+from django.test import TestCase
+from .models import User
+
+
+class RandomUserFactory(factory.django.DjangoModelFactory):
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    age = random.randint(1, 100)
+
+    class Meta:
+        model = User
+
+
+# class AdultUserFactory(factory.django.DjangoModelFactory):
+#     first_name = factory.Faker('first_name')
+#     last_name = factory.Faker('last_name')
+#     age = random.randint(18, 100)
+#
+#     class Meta:
+#         model = User
+
+
+# class YoungUserFactory(factory.django.DjangoModelFactory):
+#     first_name = factory.Faker('first_name')
+#     last_name = factory.Faker('last_name')
+#     age = random.randint(1, 17)
+#
+#     class Meta:
+#         model = User
+
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = RandomUserFactory.create()
+        # cls.adult_user = AdultUserFactory.create()
+        # cls.young_user = YoungUserFactory.create()
+
+    def test_user(self):
+        self.assertIsInstance(self.user, User)
+
+    # def test_user_age(self):
+    #     self.assertIsNotNone(self.user.age)
+    #
+    # def test_adult_user(self):
+    #     self.assertTrue(self.adult_user.is_adult())
+    #     self.assertFalse(self.young_user.is_adult())
+
+
+class UserViewSetTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = RandomUserFactory.create()
+
+    def test_get_user_detail(self):
+        resp = self.client.get(f'/users/{self.user.id}/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.json().get('first_name'), self.user.first_name)
+
+    def test_user_create(self):
+        user_data = {
+            'first_name': 'Nata',
+            'last_name': 'Natakhina',
+            'age': 30
+        }
+        resp = self.client.post('/users/', data=user_data)
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(resp.json().get('first_name'), user_data.get('first_name'))
+
+    def test_user_delete(self):
+        resp = self.client.delete(f'/users/{self.user.id}/')
+        self.assertEqual(resp.status_code, 204)
--- homework_04.py
+++ homework_04.py
@@ -1,10 +1,36 @@
-text = input("Enter a text: ")
-if text.isdigit():
-    print("This is a number.")
-    if int(text) % 2 == 0:
-        print ("The number is even.")
-    else:
-        print ("The number is odd.")
-else:
-    print(f"This is a word. It consists of {len(text)} letters.")
+import unittest
+import random
+import string
 
+def text_analysis(text):
+    if text.isdigit():
+        print("This is a number.")
+        if int(text) % 2 == 0:
+            return "The number is even."
+        else:
+            return "The number is odd."
+    else:
+        return f"This is a word. It consists of {len(text)} letters."
+
+
+class MyTestClass(unittest.TestCase):
+    def test_text_analysis(self):
+        random_even_number = random.randint(1, 100000) * 2
+        random_odd_number = random_even_number - 1
+
+        characters = string.ascii_letters + string.punctuation + string.whitespace
+        number = random.randint(1, 20)
+        random_string = ''.join(random.choice(characters) for i in range(number))
+        self.assertEqual(text_analysis(random_even_number), "The number is even.")
+        self.assertEqual(text_analysis(random_odd_number), "The number is odd.")
+        self.assertEqual(text_analysis(random_string), f"This is a word. It consists of {number} letters.")
+
+    def test_incorrect_values(self):
+        bools = [True, False]
+        random_bool = random.choice(bools)
+        with self.assertRaises(TypeError):
+            text_analysis(random_bool)
+
+
+if __name__ == '__main__':
+    unittest.main()
