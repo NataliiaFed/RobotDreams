70f17925fb4405be36e8d2b69922d3d4bfc6c7fe
NataliiaFed <nataliiafedorets@gmail.com>
2023-05-26 20:45:40 +0100

    home work 47

--- django/myRobot/settings.py
+++ django/myRobot/settings.py
@@ -94,9 +94,13 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myRobot",
+        "USER": "myRobot",
+        "PASSWORD": 'myRobot',
+        "HOST": "0.0.0.0",
+        "PORT": "5432",
     }
 }
 
